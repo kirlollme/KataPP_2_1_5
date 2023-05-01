de37f4fdceb4dbeb82a1a673dfb665f6d9fdcb72
Kirill <kir.adam2000@gmail.com>
2023-05-01 17:39:47 +0300

    решенная задача

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
 
+    private final Duck5 duck;
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + duck.toString();
+    }
+    @Autowired
+    public Rabbit4(Duck5 duck) {
+        this.duck = duck;
     }
 }
